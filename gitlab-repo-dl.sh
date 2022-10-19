6bfd00cd6b19e928e7264d3b03c5bbcf168c3b14
brunnelu <lucas.brunner@urb-x.ch>
2022-10-19 11:15:19 +0200

    updated grep x-total-pages.  Newer gitlab instances respond with lowercase Attributes.

--- gitlab-repo-dl.sh
+++ gitlab-repo-dl.sh
@@ -41,8 +41,8 @@ if [ "$1" == "group" ]; then
     done
 elif [ "$1" == "all-repo-list" ]; then
     # Get total number of pages (with 20 projects per page) from HTTP header
-    TOTAL_PAGES=`curl "$GITLAB_URL/api/v4/projects?private_token=$GITLAB_TOKEN&membership=true" -sI | grep x-total-pages | awk '{print $2}' | sed 's/\\r//g'`
-    
+    TOTAL_PAGES=`curl "$GITLAB_URL/api/v4/projects?private_token=$GITLAB_TOKEN&membership=true" -sI | grep X-Total-Pages | awk '{print $2}' | sed 's/\\r//g'`
+     
     for ((PAGE_NUMBER = 1; PAGE_NUMBER <= TOTAL_PAGES; PAGE_NUMBER++)); do
         # echo git@instance:namespace/repo.git
         curl "$GITLAB_URL/api/v4/projects?private_token=$GITLAB_TOKEN&per_page=20&page=$PAGE_NUMBER&membership=true" | jq '.[] | .ssh_url_to_repo' | sed 's/"//g'
